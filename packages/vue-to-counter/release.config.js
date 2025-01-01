3c6188d1ad66bf2ad004fd711bea6c496b72490f
siaikin <abc1310054026@outlook.com>
2025-01-01 15:42:24 +0800

    feat: Add GitHub Action

---
+++ packages/vue-to-counter/release.config.js
@@ -0,0 +1,9 @@
+export default {
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
+    "@semantic-release/github",
+  ],
+  repositoryUrl: "https://github.com/siaikin/vue-to-counter.git",
+};
